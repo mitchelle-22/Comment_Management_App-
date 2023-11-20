f77c44a465a811593af198310a4b13701c0d241a
Mich <mitchellefetty@gmail.com>
2023-11-20 16:49:28 +0200

    tab view

--- Comment_Management_App/Comment_Management_AppApp.swift
+++ Comment_Management_App/Comment_Management_AppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Comment_Management_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CommentManagementTabView()
         }
     }
 }
