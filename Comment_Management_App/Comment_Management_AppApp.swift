e0dc6c0009d6a575e2295b3f4805c44973291b1d
Mich <mitchellefetty@gmail.com>
2023-11-20 16:42:23 +0200

    Initial Commit

---
+++ Comment_Management_App/Comment_Management_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comment_Management_AppApp.swift
+//  Comment_Management_App
+//
+//  Created by MIch on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Comment_Management_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Comment_Management_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Comment_Management_App
+//
+//  Created by MIch on 2023/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
