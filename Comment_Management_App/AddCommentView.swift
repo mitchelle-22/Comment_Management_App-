1a19e4e82e6ffa9c3075fe0640e9e564ca4062d2
Mich <mitchellefetty@gmail.com>
2023-11-20 17:09:12 +0200

    tab view done

---
+++ Comment_Management_App/AddCommentView.swift
@@ -0,0 +1,18 @@
+//
+//  AddCommentView.swift
+//  Comment_Management_App
+//
+//  Created by MIch on 2023/11/20.
+//
+
+import SwiftUI
+
+struct AddCommentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCommentView()
+}
--- Comment_Management_App/CommentManagementTabView.swift
+++ Comment_Management_App/CommentManagementTabView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct CommentManagementTabView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView{
+            CommentManagementListView()
+                   .tabItem {
+                       Image(systemName: "house.fill")
+                       Text("Home")
+                   }
+               AddCommentView()
+                   .tabItem {
+                       Image(systemName: "person.crop.circle.fill")
+                       Text("Profile")
+                   }
+               ProfileView()
+               .tabItem{
+                       Image(systemName: "bag.circle.fill")
+                       Text("Order")
+                   }
+               
+
+                   }
+        .accentColor(Color("mainColor"))
+           
+           
     }
 }
 
