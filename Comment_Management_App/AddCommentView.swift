cac59b999c509d7da02dd5a7dd4482744fc0dc5e
Mich <mitchellefetty@gmail.com>
2023-11-21 10:30:59 +0200

    Add comment view added

--- Comment_Management_App/AddCommentView.swift
+++ Comment_Management_App/AddCommentView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct AddCommentView: View {
+    @State private var newCommenText: String = ""
+    @StateObject var viewModel = CommentViewModel()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Enter your comment:")
+                .font(.headline)
+                .padding()
+            
+            TextEditor(text: $newCommenText)
+                .padding()
+                .background(Color.gray.opacity(0.1))
+                .cornerRadius(8)
+            
+            Button(action: {
+                let newComment = Comment(id: 0,name:"Michelle",email:"mich@gmail.com",body: newCommenText)
+                viewModel.postComment(comment: newComment){
+                    error in if let error = error{
+                        print("Error post comment:\(error.localizedDescription)")
+                    }else{
+                        print("Comment posted successfully!")
+                        newCommenText = ""
+                    }
+                }
+            }){
+                Text("Post")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding()
+        }
+        .navigationTitle("New Post")
+        .padding()
+        .onAppear{
+            viewModel.fetchComments()
+        }
     }
 }
 
--- Comment_Management_App/CommentViewModel.swift
+++ Comment_Management_App/CommentViewModel.swift
@@ -18,7 +18,7 @@ class CommentViewModel : ObservableObject{
         }
         URLSession.shared.dataTask(with: url)
         {
-            data,response,error in 
+            data,response,error in
             guard let data = data else {return}
             
             do{
@@ -30,5 +30,35 @@ class CommentViewModel : ObservableObject{
                 print("Error decoding comments:\(error)")
             }
         }.resume()
+    }
+    
+    func postComment(comment: Comment,completion: @escaping(Error?)->Void)
+    {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/comments")else{
+            completion(NSError(domain: "Invalid URL", code: 0,userInfo: nil))
+            return
+            
+        }
+        
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do{
+            let jsonData = try JSONEncoder().encode(comment)
+            request.httpBody = jsonData
+            
+            URLSession.shared.dataTask(with: request){
+                data,response,error in
+                if let error = error{
+                    completion(error)
+                    return
+                }
+                completion(nil)
+            }.resume()
+        }catch{
+            completion(error)
+        }
     }
 }
